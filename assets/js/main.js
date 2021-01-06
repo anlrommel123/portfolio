2563191958057fd874dfb0f3a69207999f59a8e3
Rommel S. Belbestre <rsbelbestre@virginiafood.com.ph>
2021-01-06 16:06:41 +0800

    layout body

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(function(){
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
-    $('a.nav-link.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
                 this.pathname.replace(/^\//, "") &&
