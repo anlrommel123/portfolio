bd32f5555b8a61c5f55513026a7a0c1d7d660e34
Rommel S. Belbestre <rsbelbestre@virginiafood.com.ph>
2021-01-06 12:13:12 +0800

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+$(function(){
+    "use strict"; // Start of use strict
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    
+    // Collapse now if page is not at top
+    navbarCollapse();
+
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+
+});
